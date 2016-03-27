8eb04c0e84413a1da016522b7b1cbb2597435ab0
Liu's DeskTop <wwwlsmcom@msn.cn>
2016-03-27 23:03:23 +0800

    add paredit
    which can edit lisp effectively

--- layers/liu233w/packages.el
+++ layers/liu233w/packages.el
@@ -35,6 +35,7 @@
     multiple-cursors
     smart-compile
     slime
+    paredit
     )
   "The list of Lisp packages required by the liu233w layer.
 
@@ -74,6 +75,24 @@ Each entry is either:
                                ((spacemacs/system-is-linux) "sbcl")
                                (t "sbcl")
                                ))
+  )
+
+(defun liu233w/init-paredit ()
+  (use-package paredit)
+  (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+  (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+  (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+  (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+  (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+  (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+  (add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
+  ;; Stop SLIME's REPL from grabbing DEL,
+  ;; which is annoying when backspacing over a '('
+  (defun override-slime-repl-bindings-with-paredit ()
+    (define-key slime-repl-mode-map
+      (read-kbd-macro paredit-backward-delete-key) nil))
+  (add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit)
   )
 
 ;;; packages.el ends here
