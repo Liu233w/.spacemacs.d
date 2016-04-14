d2879b99c716eb0c5fefd6ba119c1f426f24fcd6
Liu's DeskTop <wwwlsmcom@msn.cn>
2016-04-14 23:33:53 +0800

    fix org-page behavior in windows

--- layers/org-page+/keybindings.el
+++ layers/org-page+/keybindings.el
@@ -1,5 +1,5 @@
 (evil-leader/set-key "opp"
   '(lambda () (interactive) "publish your blog automatically"
-    (op/do-publication nil nil nil t nil)))
+    (op/do-publication nil nil nil (if (spacemacs/system-is-linux) t nil) nil)))
 
 (evil-leader/set-key "opn" 'op/new-post)
