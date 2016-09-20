52de9813971465abdfddaab5d40890811de6107e
surface-of-liu <wwwlsmcom@outlook.com>
2016-09-20 16:27:08 +0800

    1

--- on-test.el
+++ on-test.el
@@ -1,1 +1,10 @@
 ;;; 包含正在测试中的代码，还不能正式开始使用
+
+(defun liu233w//compile-and-run (cmp-cmd run-cmd)
+  "Run cmp-cmd, if success, then run run-cmd and print the result.
+Or just print the error message."
+  (let* ((process (start-process-shell-command
+                   (gensym)
+                   "*liu233w/run-current-file output*"
+                   cmp-cmd))
+         )))
