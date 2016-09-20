9d8fd275c695c4e065692935c3e6367b5000dcd9
surface-of-liu <wwwlsmcom@outlook.com>
2016-09-20 19:16:15 +0800

    ok

--- on-test.el
+++ on-test.el
@@ -1,13 +1,11 @@
 ;;; 包含正在测试中的代码，还不能正式开始使用
 
-(defvar liu233w//prev-compile-command "make -k")
+(defvar liu233w//prev-compile-command "make -k ")
 (defvar liu233w//cr-run-command "")
 
 (defun liu233w//compile-and-run (cmp-cmd run-cmd)
   "Run cmp-cmd, if success, then run run-cmd and print the result.
 Or just print the error message."
-  (make-local-variable 'liu233w//prev-compile-command)
-  (make-local-variable 'liu233w//cr-run-command)
   (setf liu233w//prev-compile-command compile-command
         liu233w//cr-run-command run-cmd)
   (compile cmp-cmd))
@@ -17,7 +15,8 @@ Or just print the error message."
     (set-window-buffer
      (get-buffer-window "*compilation*")
      (get-buffer-create "*liu233w/run-current-file output*")))
-  (setf compile-command liu233w//prev-compile-command))
+  (setf compile-command liu233w//prev-compile-command)
+  (shell-command liu233w//cr-run-command "*liu233w/run-current-file output*"))
 
 (setq compilation-exit-message-function
       'liu233w//show-compile-result)
