290d724b94827856d7b7099b23c3dacfcdb6d04c
surface-of-liu <wwwlsmcom@outlook.com>
2016-09-30 16:02:23 +0800

    在写python的教程时需要的特殊函数

--- on-test.el
+++ on-test.el
@@ -1,1 +1,9 @@
 ;;; 包含正在测试中的代码，还不能正式开始使用
+
+;;; 便于我写python讲义的函数
+(defun liu233w/py-acm-insert-src-block ()
+  (interactive)
+  (save-excursion
+    (insert "#+begin_src python :exports both
+#+end_src")))
+(evil-ex-define-cmd "pys" #'liu233w/py-acm-insert-src-block)
