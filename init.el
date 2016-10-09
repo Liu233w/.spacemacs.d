8f5dd4eedd63486ee5c08d53c5991f8f26793b37
surface-of-liu <wwwlsmcom@outlook.com>
2016-10-09 19:48:00 +0800

    添加高dpi支持

--- init.el
+++ init.el
@@ -181,8 +181,9 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Source Code Pro"
-                               :size 18
+   dotspacemacs-default-font `("Source Code Pro"
+                               :size ,(if (> (x-display-pixel-height) 1200)
+                                        30 18)
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
